d33cdf66b44590bbb23fdb7b41c53390f16ba590
Wonkyo Choe <wonkyochoe@virginia.edu>
2022-10-30 19:38:46 -0400

    add scripts

---
+++ experiments/scenario_runner/run_ours_schedule.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+hzs=(30 15)
+#detectors=(efficientdet-d2 efficientdet-d3 efficientdet-d4 efficientdet-d5 efficientdet-d6 efficientdet-d7)
+target_speeds=(15 20 25 30)
+file_prefix_name=ours_schedule
+scenarios=(ERDOSPedestrianBehindCar ERDOSPedestriansBehindCar)
+scenarios=(ERDOSPedestriansBehindCar)
+detectors=(yolox_m)
+
+cd ${PYLOT_HOME}
+dir_name=results_${file_prefix_name}_30Hz
+mkdir -p ${dir_name}
+
+for scenario in ${scenarios[@]}; do
+    for ondemand in ${detectors[@]}; do
+        for run in `seq 1 5`; do
+            for hz in ${hzs[@]}; do
+                for target_speed in ${target_speeds[@]}; do
+                    file_base=${dir_name}/${file_prefix_name}_scenario_${scenario}_${ondemand}_target_speed_${target_speed}_Hz_${hz}_run_${run}
+                    if [ ! -f "${PYLOT_HOME}/${file_base}.csv" ]; then
+                        echo "[x] Running the experiment with ADS, target speed $target_speed"
+                        cd ${PYLOT_HOME}/scripts ; ./run_simulator.sh &
+                        sleep 10
+                        cd $PYLOT_HOME ; python3 pylot.py --flagfile=configs/scenarios/ours.conf --target_speed=$target_speed --log_file_name=$file_base.log --csv_log_file_name=$file_base.csv \
+                            --profile_file_name=$file_base.json \
+                            --track_file_name=$file_base.track \
+                            --obstacle_detection_model_paths=${PYLOT_HOME}/dependencies/models/obstacle_detection/efficientdet/efficientdet-d0/efficientdet-d0_frozen.pb,${PYLOT_HOME}/dependencies/models/obstacle_detection/yolox/${ondemand}.pth \
+                            --yolox_detection_operator --with_importance --hinted_obstacles_threshold=0.6 \
+                            --enable_periodic_task --periodic_task_fps=$hz \
+                            --obstacle_detection_model_names=efficientdet-d0,${ondemand} \
+                            --simulator_fps=30 --simulator_control_frequency=30 \
+                            --simulator_camera_frequency=30 --simulator_imu_frequency=10 --simulator_localization_frequency=10 &
+                        cd $SCENARIO_RUNNER_HOME ; python3 scenario_runner.py --scenario $scenario --reloadWorld --timeout 600
+                        echo "[x] Scenario runner finished. Killing Pylot..."
+                        pkill --signal 9 -f scenario_runner.py
+                        pkill --signal 9 pylot.py
+                        # Kill the simulator
+                        `ps aux | grep CarlaUE4 | awk '{print $2}' | head -n -1 | xargs kill -9`
+                        `ps aux | grep CarlaUE4 | awk '{print $2}' | tail -n -1 | xargs kill -9`
+                        sleep 40
+                    else
+                        echo "$file_base exists"
+                    fi
+                done
+            done
+        done
+    done
+done
